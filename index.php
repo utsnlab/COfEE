64eadd33c2d0193b78259969247b9893ca9ecde1
mham <mhahmadim@gmail.com>
2021-01-02 18:10:46 +0330

    add event arguments

--- index.php
+++ index.php
@@ -214,6 +214,6 @@ if(!(isset($_SESSION['user']['id']) and is_numeric($_SESSION['user']['id']))){
     <?php else: ?>
         <script src="template/bootstrap/ltr/js/bootstrap.min.js"></script>
     <?php endif; ?>
-    <script src="template/custom.js?1"></script>
+    <script src="template/custom.js?2"></script>
 </body>
 </html>
